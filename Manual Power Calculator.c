d6cd5fc10131a3dc113a8e8f86dbfa20d4d5ff10
nithishkumarkumar16-jpg <nithishkumarkumar16@gmail.com>
2026-02-06 14:19:30 +0530

    Implement manual power calculation in C in c

---
+++ Manual Power Calculator.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int a,b,c;
+    scanf("%d%d",&a,&b);
+    c=a;
+    for(int i=0;i<b-1;i++){
+        a=c*a;
+    }
+    if(b==0){
+        a=1;
+    }
+    printf("%d",a);
+
+   
+    return 0;
+}
