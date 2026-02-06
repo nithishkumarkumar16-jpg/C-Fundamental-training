4e5b0fc2ebd86342185e83ae3d1bdb3d93ccfb8c
nithishkumarkumar16-jpg <nithishkumarkumar16@gmail.com>
2026-02-06 14:27:11 +0530

    Add prime factors program in prime factors all.c
    Implement a program to print all prime factors of a number.

---
+++ prime factors all.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int a,i;
+    scanf("%d",&a);
+    for(i=1;i<=a;i++){
+        if(a%i==0){
+            printf("%d ",i);     
+        }
+    }
+
+    return 0;
+}
