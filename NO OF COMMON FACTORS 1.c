8be642fb383e59951fba96f37eeafb9575a977b8
nithishkumarkumar16-jpg <nithishkumarkumar16@gmail.com>
2026-02-06 14:14:09 +0530

    Add program to count common factors in c
    Implement a program to count common factors of two integers.

---
+++ NO OF COMMON FACTORS 1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int count=0,a,b;
+    for(int i=1;i<=a;i++){
+        if(a%i==0 && b%i==0){
+          count ++;    
+        }
+    }
+
+    return count;
+}
